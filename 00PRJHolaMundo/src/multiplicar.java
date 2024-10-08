65cb5addd76d09d894a27a247af6da3ae662fa11
JRGamer234 <ramosgomezjorge1@gmail.com>
2024-10-08 09:27:56 +0200

    1.

--- 00PRJHolaMundo/src/multiplicar.java
+++ 00PRJHolaMundo/src/multiplicar.java
@@ -14,29 +14,69 @@ public class multiplicar {
 		* formato: 3 * 1 = 3
 		* */
 
-		int num;
+		int num, i;
 		System.out.println("Numero: ");
 		num = teclado.nextInt();
 		
-		for(num; num >= 10;num++) {
-			System.out.println(num);
-		}
+		for (i = 1; i <= 10; i++) {
+            System.out.println(num + " * " + i + " = " + (num * i));
+        }
 		
 		/** BUCLEFOREJER
 		 * Programa que solicita por teclado un número
 		 * y indica si este es primo o no lo es.
 		 * */
-
+		
+		int num1;
+		System.out.println("Numero(primo): ");
+		num1 = teclado.nextInt();
+		
+		boolean esPrimo = true;
+		
+		for(int x = 2; x < num1; x++) {
+			if(num1 <= 1) {
+			esPrimo = false;
+			break;
+			}
+		}
+			
+		
+		
+		
 		/** BUCLEFOREJER2
 		* Solicite dos numeros (inicio y fin) e imprima por
 		* pantalla la cantidad de numeros impares entre ellos
 		* */
+		int numi1, numi2, contador = 0;
+		System.out.println("Numero Inicio: ");
+		numi1 = teclado.nextInt();
+		System.out.println("Numero Fin: ");
+		numi2 = teclado.nextInt();
+		
+		numi1++;
+		for(int j = numi1; j < numi2; j++) {
+            if (j % 2 != 0) {
+                contador++;
+            }
+        }
+		System.out.println(contador);
 
+		
+		
 		/** BUCLEFOREJER3
 		/**
 		 * Dibuja un cuadrado en pantalla solo haciendo uso de: System.out.println("*");
 		 * System.out.print("*"); Ejemplo : * * * * * * * * *
 		 */
-
+		int tama;
+		System.out.println("Tamano del cuadrado: ");
+		tama = teclado.nextInt();
+		 for(int a = 0; a < tama; a++) {
+	            for(int b = 0; b < tama; b++) {
+	                System.out.print("* ");
+	            }
+	            System.out.println();
+	        }
+		
 }
 }
