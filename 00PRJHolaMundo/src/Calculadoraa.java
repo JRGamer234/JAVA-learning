1e2e9ab2524a74b4d3b26ab6d35f35ad34599a08
JRGamer234 <ramosgomezjorge1@gmail.com>
2024-11-04 12:08:23 +0100

    º

---
+++ 00PRJHolaMundo/src/Calculadoraa.java
@@ -0,0 +1,26 @@
+
+public class Calculadoraa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public int sumar(int num1, int num2) {
+			return num1 + num2;
+		}
+		
+		public int restar(int num1, int num2) {
+			return num1 - num2;
+		}
+		
+		public int multiplicar(int num1, int num2) {
+			return num1 * num2;
+		}
+		
+		public intdividir(int num1, int num2) {
+			if(num1 == 0 || num2 == 0) {
+				break;
+			}
+			return num1 / num2;
+		}
+	}
+
+}
---
+++ 00PRJHolaMundo/src/funciones.java
@@ -0,0 +1,10 @@
+
+public class funciones {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculadora miCalculadoraa = new Calculadora();
+		System.out.println(miCalculadoraa.sumar(1,1));
+	}
+
+}
