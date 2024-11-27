2acbd83b5326949b00a9f7ed8d697d9cdb3a2168
JRGamer234 <ramosgomezjorge1@gmail.com>
2024-11-27 10:48:12 +0100

    1

--- 00PRJHolaMundo/src/ExamenPractica1/Ejercicios.java
+++ 00PRJHolaMundo/src/ExamenPractica1/Ejercicios.java
@@ -51,6 +51,96 @@ public class Ejercicios {
 		*/
 		
 		
+		 Scanner s = new Scanner(System.in);
+
+	        int botes = 0;
+	        boolean prime = false;
+	        int sprime = 0;
+	        int precio = 4;
+	        
+	       
+	        
+	        System.out.println("Cuantos botes deseas comprar?");
+	        System.out.print("--> ");
+	        botes = s.nextInt();
+	        
+	 
+	        
+	        while(sprime != 1 || sprime != 2) {
+	        	
+	        System.out.println("Cliente PRIME?");
+	        System.out.println("1. SI");
+	        System.out.println("2. NO");
+	        sprime = s.nextInt();
+
+	        
+	        if (sprime == 1) {
+	            prime = true;
+	            break;
+	        } else if (sprime == 2) {
+	            prime = false;
+	            break;
+	        } else {
+	            System.out.println("Opción inválida. Debes elegir 1 o 2.");
+	        }
+	      }
+	        
+	        
+	       
+
+
+	        if (prime) {
+	            if (botes > 6) {
+	            	
+	                System.out.println("El precio total es: " + (precio * botes));
+	                float nuevoprecio = precio * botes;
+	                System.out.println("Botes totales añadiendo regalo por cortesía de Amazon: " + (botes + 3));
+	                System.out.println("Descuento: " + (nuevoprecio * 0.05));
+	                System.out.println("Aplicando descuento del prime: " + (nuevoprecio - (nuevoprecio * 0.05)));
+	                
+	            } else if (botes == 1 || botes == 2) {
+	            	
+	            	System.out.println("El precio total es: " + (precio * botes));
+	                float nuevoprecio = precio * botes;
+	                System.out.println("Botes totales: " + botes);
+	                System.out.println("Descuento: " + (nuevoprecio * 0.05));
+	                System.out.println("Aplicando descuento del prime: " + (nuevoprecio - (nuevoprecio * 0.05)));
+	                
+	            } else if (botes > 2 && botes < 5) {
+	            	
+	            	System.out.println("El precio total es: " + (precio * botes));
+	                float nuevoprecio = precio * botes;
+	                System.out.println("Botes totales añadiendo regalo por cortesía de Amazon: " + (botes + 1));
+	                System.out.println("Descuento: " + (nuevoprecio * 0.05));
+	                System.out.println("Aplicando descuento del prime: " + (nuevoprecio - (nuevoprecio * 0.05)));
+	                
+	            } else if (botes >= 5 && botes <= 6) {
+	            	
+	            	System.out.println("El precio total es: " + (precio * botes));
+	                float nuevoprecio = precio * botes;
+	                System.out.println("Botes totales añadiendo regalo por cortesía de Amazon: " + (botes + 2));
+	                System.out.println("Descuento: " + (nuevoprecio * 0.05));
+	                System.out.println("Aplicando descuento del prime: " + (nuevoprecio - (nuevoprecio * 0.05)));
+	            }
+	        } else {
+	            if (botes > 6) {
+	                System.out.println("El precio total es: " + precio * botes);
+	                System.out.println("Botes totales añadiendo regalo por cortesía de Amazon: " + (botes + 3));
+	            } else if (botes == 1 || botes == 2) {
+	                System.out.println("El precio total es: " + precio * botes);
+	            } else if (botes > 2 && botes < 5) {
+	                System.out.println("El precio total es: " + precio * botes);
+	                System.out.println("El precio total es: " + precio * botes);
+	                System.out.println("Botes totales añadiendo regalo por cortesía de Amazon: " + (botes + 1));
+	            } else if (botes >= 5 && botes <= 6) {
+	                
+	                System.out.println("El precio total es: " + precio * botes);
+	                System.out.println("Botes totales añadiendo regalo por cortesía de Amazon: " + (botes + 2));
+	            }
+	        }
+
+		
+		
 		
 		
 		
