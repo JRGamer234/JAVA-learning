4fab9c37da90f25ca2a23f5893caf7f6fe0c1939
JRGamer234 <ramosgomezjorge1@gmail.com>
2025-02-17 12:09:49 +0100

    1

--- 00PRJHolaMundo/src/T2/TresEnRaya.java
+++ 00PRJHolaMundo/src/T2/TresEnRaya.java
@@ -2,44 +2,72 @@ package T2;
 import java.util.Scanner;
 
 public class TresEnRaya {
-
-	private char[][] tablero;
-	private int pos_f, pos_c;
-	
-	public TresEnRaya() {
-		tablero = new char[3][3];
-		for (int i = 0; i < tablero.length ;i++) {
-			for(int j = 0; j < tablero.length; j++) {
-				tablero[i][j] = '_';
-			}
-		}
-	}
-	
-	public void imprimir() {
-		for (int i = 0; i < tablero.length ;i++) {
-			for(int j = 0; j < tablero.length; j++) {
-				System.out.println("|" + tablero[i][j]);
-			}
-			System.out.println("|");
-		}
-	}
-	
-	public void colocar_ficha() {
-		Scanner teclado = new Scanner(System.in);
-		
-		System.out.println("Introduce posicion de la fila: ");
-		pos_f = teclado.nextInt();
-		System.out.println("Introduce posicion de la columna: ");
-		pos_c = teclado.nextInt();
-		tablero[pos_f][pos_c] = "X";
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		TresEnRaya tres = new TresEnRaya();
-		tres.imprimir();
-		tres.colocar_ficha();
-		tres.imprimir();
-	}
-
+    private char[][] tablero = new char[3][3];
+    private boolean turnoX = true; // true para X, false para O
+    
+    public TresEnRaya() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                tablero[i][j] = '_';
+            }
+        }
+    }
+    
+    public void imprimir() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(tablero[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    
+    public boolean colocarFicha(int fila, int columna) {
+        if (fila < 0 || fila >= 3 || columna < 0 || columna >= 3 || tablero[fila][columna] != '_') {
+            System.out.println("Posición inválida. Intenta de nuevo.");
+            return false;
+        }
+        tablero[fila][columna] = turnoX ? 'X' : 'O';
+        turnoX = !turnoX;
+        return true;
+    }
+    
+    public boolean hayGanador() {
+        for (int i = 0; i < 3; i++) {
+            if (tablero[i][0] != '_' && tablero[i][0] == tablero[i][1] && tablero[i][1] == tablero[i][2]) return true;
+            if (tablero[0][i] != '_' && tablero[0][i] == tablero[1][i] && tablero[1][i] == tablero[2][i]) return true;
+        }
+        if (tablero[0][0] != '_' && tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2]) return true;
+        if (tablero[0][2] != '_' && tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0]) return true;
+        return false;
+    }
+    
+    public void jugar() {
+        Scanner teclado = new Scanner(System.in);
+        int turnos = 0;
+        while (turnos < 9) {
+            imprimir();
+            System.out.println("Turno de " + (turnoX ? "X" : "O") + " (fila columna):");
+            int fila = teclado.nextInt();
+            int columna = teclado.nextInt();
+            if (colocarFicha(fila, columna)) {
+                if (hayGanador()) {
+                    imprimir();
+                    System.out.println("¡El jugador " + (turnoX ? "O" : "X") + " ha ganado!");
+                    break;
+                }
+                turnos++;
+            }
+        }
+        if (turnos == 9) {
+            imprimir();
+            System.out.println("¡Empate!");
+        }
+        teclado.close();
+    }
+    
+    public static void main(String[] args) {
+        new TresEnRaya().jugar();
+    }
 }
